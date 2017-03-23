76c9acf516bf51529f2de3b62b0f8666e76d496a
lindaxiao <lindaxiao.hust@gmail.com>
2017-03-23 12:20:57 +0800

    practice more

--- js8.js
+++ js8.js
@@ -6,10 +6,44 @@ var rl = readline.createInterface({
   output: process.stdout
 })
 
+var result = ''
 rl.on('line', function(line) {
   var n = +line
+  for(var i = 2; ; i++) {
+    var tmp = Math.pow(n, 1.0/i)
+    if(tmp < 2) {
+      result = 'No'
+      break
+    } else if(!check(tmp)) {
+      continue
+    } else if(!isPrime(tmp)) {
+      continue
+    } else {
+      result = tmp + ' ' + i
+      break
+    }
+  }
+  console.log(result);
+
 })
 
-function isPrim(x) {
-  
+// 判断是否为素数
+function isPrime(x) {
+  if(x < 2) {
+    return false
+  }
+  for(var i = 2; i <= Math.sqrt(x); i++) {
+    if(x%i === 0) {
+      return false
+    }
+  }
+  return true
+}
+
+// 检查是否为整数
+function check(x) {
+  if(Math.floor(x) === Math.ceil(x)) {
+    return true
+  }
+  return false
 }
